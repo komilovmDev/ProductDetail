9ccab9eeeb6dc63d9b9ba54aa5968a997225ac43
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-03-22 15:09:46 +0500

    product dtl function fulled

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -26,28 +26,3 @@ export default function Home() {
     </>
   )
 }
-
-// import './style.css'
-// import ProductData from '../../utils/ProductData'
-// import { Link } from 'react-router-dom'
-
-// export default function Home() {
-//   return (
-//     <>
-//       <div className="titleCon">
-//         <span className="title">Nike shop</span>
-//       </div>
-//       <div className="products">
-//         {ProductData.map((product) => (
-//           <div className="product" key={product.id}>
-//             <Link to={`/products/${product.id}`}>
-//               <img src={product.image} alt={product.name} />
-//             </Link>
-//             <p>{product.name}</p>
-//             <span>${product.price}</span>
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
--- client/src/components/productDetail/ProductDtl.jsx
+++ client/src/components/productDetail/ProductDtl.jsx
@@ -5,7 +5,9 @@ import './style.css'
 export default function ProductDtl() {
 
     const { productId } = useParams();
-    const dtlProduct = ProductData.find((prop) => prop.id === productId) ;
+    const dtlProduct = ProductData.find((prop) => prop.id == productId) ;
+
+    console.log('Hello');
 
     return (
       <div className="productBox">
@@ -16,20 +18,3 @@ export default function ProductDtl() {
     )
 }
 
-
-// import { useParams } from 'react-router-dom';
-// import ProductData from '../../utils/ProductData';
-// import './style.css'
-
-// export default function ProductDtl() {
-//   const { productId } = useParams();
-//   const dtlProduct = ProductData.find((prop) => prop.id === productId);
-
-//   return (
-//     <div className="productBox">
-//       <img src={dtlProduct.image} alt="" />
-//       <p>{dtlProduct.name}</p>
-//       <span>${dtlProduct.price}</span>
-//     </div>
-//   );
-// }
